7f54c71655d711e9f354c70838ec795b04138556
evanvd <ivanavdeev7347@gmail.com>
2025-09-27 17:43:53 +0300

    makefile change

--- include/sort.h
+++ include/sort.h
@@ -8,4 +8,4 @@
 int FirstLetterCompare(const void* n1, const void* n2);
 int LastLetterCompare(const void* n1, const void* n2);
 void BubbleSort(void** data, size_t length, comparison_fn_t comparator); 
-void swap(char** string1, char** string2);
+void swap(void** string1, void** string2);
